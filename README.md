2d485faa8641183549afeeedd48804b81457e67e
R. Teal Witter <rtealwitter@gmail.com>
2020-10-26 11:28:09 -0600

    fixed bug in downloading script

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Final project for Professor Mohri's Fall 2020
 Foundations of Machine Learning.
 
 # Data
+Make sure you have a folder called `trips` at the top level
+of this repo.
 To download the data, run `bash getdata.sh` on the command
-line from the top level of this repo.
+line.
 GitHub will automatically ignore all `.csv` files.
--- getdata.sh
+++ getdata.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
-rm -r trips
-mkdir trips
+
+download () {
+    echo "Checking $1..."
+    url=https://s3.amazonaws.com/tripdata/$1-citibike-tripdata.zip
+    if curl --head -silent --fail $url; then
+    if test -f "trips/$1.csv"; then
+    echo "$1.csv already exists..."
+    else
+    curl -o trips/$1-citibike-tripdata.zip $url
+    unzip -p trips/$1-citibike-tripdata.zip > trips/$1.csv
+    rm trips/$1-citibike-tripdata.zip
+    fi fi
+}
 
 for year in {2013..2020}
 do
-for month in 01 02 03 04 05 06 07 08 09 10 11 12
+for month in {1..12}
 do
-name=$year$month
-echo $name
-url=https://s3.amazonaws.com/tripdata/$name-citibike-tripdata.zip
-if curl --head -silent --fail $url
-then
-curl -o trips/$name-citibike-tripdata.zip $url
-unzip -p trips/$name-citibike-tripdata.zip > trips/$name.csv
-rm trips/$name-citibike-tripdata.zip
-fi
+for name in $year$month "${year}0${month}"
+do
+download $name
+done
 done
 done
