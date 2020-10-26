16c78216f4321d575305283e64be55a1508daacc
R. Teal Witter <rtealwitter@gmail.com>
2020-10-26 11:52:48 -0600

    stupid differing file names

--- getdata.sh
+++ getdata.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 download () {
-    echo "Checking $1..."
-    url=https://s3.amazonaws.com/tripdata/$1-citibike-tripdata.zip
-    if curl --head -silent --fail $url; then
+    echo "Checking $1..."    
+    if curl --head -silent --fail $3; then
     if test -f "trips/$2.csv"; then
     echo "$2.csv already exists..."
     else
-    curl -o trips/$1-citibike-tripdata.zip $url
+    curl -o trips/$1-citibike-tripdata.zip $3
     unzip -p trips/$1-citibike-tripdata.zip > trips/$2.csv
     rm trips/$1-citibike-tripdata.zip
     fi fi
@@ -19,8 +18,11 @@ for month in {1..12}
 do
 for name in $year$month "${year}0${month}"
 do
+for url in https://s3.amazonaws.com/tripdata/$name-citibike-tripdata.zip https://s3.amazonaws.com/tripdata/$name-citibike-tripdata.csv.zip
+do
 filename="${year}_${month}"
-download $name $filename
+download $name $filename $url
+done
 done
 done
 done
