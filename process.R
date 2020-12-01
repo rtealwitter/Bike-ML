69b21ce04e7e2f3c8d88c83c94daac812b4ce0d9
R. Teal Witter <rtealwitter@gmail.com>
2020-12-01 09:13:31 -0700

    only one copy of weather columns now

--- process.R
+++ process.R
@@ -1,16 +1,15 @@
 library(dplyr)
 
-combined1 <- read.csv("data/input.csv")
+data <- read.csv("data/data.csv")
 weather <- read.csv("data/weather.csv")
 
-combined1$standarddate <- as.Date(combined1$date, "%m/%d/%Y")
+data$standarddate <- as.Date(data$date, "%m/%d/%Y")
 weather$standarddate <- as.Date(weather$DATE, "%m/%d/%y")
 
 skinyweather <- weather %>%
   select(standarddate, AWND, PRCP, SNOW, SNWD, TAVG, TMAX, TMIN, WDF2, WSF2)
 
-combined2 <- inner_join(combined1, skinyweather, by="standarddate")
+data1 <- inner_join(data, skinyweather, by="standarddate")
 
-write.csv(combined2, "data/data.csv")
-
+write.csv(data1, "data/data.csv")
 
