199e2d4886d16b4e4c1a70cf46103a4de8240f3f
R. Teal Witter <rtealwitter@gmail.com>
2020-12-01 10:33:45 -0700

    uncompress not compress

--- handledata.sh
+++ handledata.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Split and compress
-split -l 1000000 data/data.csv data/data.csv
-tar -czvf data/dataaa.tar.gz data/data.csvaa
-tar -czvf data/dataab.tar.gz data/data.csvab
-tar -czvf data/dataac.tar.gz data/data.csvac
+## Split and compress
+#split -l 1000000 data/data.csv data/data.csv
+#tar -czvf data/dataaa.tar.gz data/data.csvaa
+#tar -czvf data/dataab.tar.gz data/data.csvab
+#tar -czvf data/dataac.tar.gz data/data.csvac
 
-## Combine and uncompress
-#tar -zxvf data/dataaa.tar.gz
-#tar -zxvf data/dataab.tar.gz
-#tar -zxvf data/dataac.tar.gz
-#cat data/data.csv* > data/data.csv
+# Combine and uncompress
+tar -zxvf data/dataaa.tar.gz
+tar -zxvf data/dataab.tar.gz
+tar -zxvf data/dataac.tar.gz
+cat data/data.csv* > data/data.csv
