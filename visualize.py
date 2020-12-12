dcb256d7a23c75f759c4b5666e4a6bd67be42722
R. Teal Witter <rtealwitter@gmail.com>
2020-12-12 12:13:42 -0700

    visualization

--- visualize.py
+++ visualize.py
@@ -6,19 +6,24 @@ def importfile(filename):
             results = eval(line.replace('\n', ''))
     return results
 
-def plot(results, keyfilter, title):
+def plot(results, keyfilter, title, filename):
     for key in results:
         for key2 in results[key]:
-            if key[1] == keyfilter:
+            if key[0] == keyfilter:
                 data = results[key][key2]
-                name = "{} ({})".format(key[0], key2)
+                if key[1] == 'None':
+                    name = 'Balanced Margin ({})'.format(key2)
+                if key[1] == 'Margin':
+                    name = 'Imbalanced Margin ({})'.format(key2)
                 plt.plot(list(range(1,len(data)+1)), data, label=name)
     plt.xlabel('Iterations')
     plt.ylabel('Error')
     plt.title(title)
     plt.ylim(0)
     plt.legend()
-    plt.show()
+    plt.savefig(filename)
+    plt.clf()
 
 results = importfile(filename = 'boostingresults.txt')
-plot(results, keyfilter='Margin', title='SVM Boosting with Imbalanced Margins')
+plot(results, keyfilter='Reweight', title='AdaBoost with SVM Weak Learner and Reweighted Data', filename='graphics/reweighted.pdf')
+plot(results, keyfilter='Resample', title='AdaBoost with SVM Weak Learner and Resampled Data', filename='graphics/resampled.pdf')
